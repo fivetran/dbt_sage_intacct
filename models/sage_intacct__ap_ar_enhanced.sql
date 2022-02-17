928e9944bacbf1d5be0d861325b3061d4fac1567
fivetran-reneeli <renee.li@fivetran.com>
2022-02-16 16:58:15 -0800

    update

--- models/sage_intacct__ap_ar_enhanced.sql
+++ models/sage_intacct__ap_ar_enhanced.sql
@@ -1,22 +1,41 @@
-with general_ledger as (
-    select *
-    from {{ref('sage_intacct__general_ledger')}}
+with gl_detail as (
+    select * 
+    from {{ ref('stg_sage_intacct__gl_detail') }} 
 ),
 
-    select
-    
-	transaction,
-	entry_date,
-    when_created ,
-    when_due,
-    when_modified,
-    when_paid,
-	customer,
-	customer id,
-	vendor ,
-	vendor id,
-	location,
-	location id
+gl_account as (
+    select * 
+    from {{ ref('int_sage_intacct__account_classifications') }} 
+),
 
+enhanced as (
+
+    
+
+    select
+
+	gld.transaction,
+	gld.entry_date,
+    gld.when_created ,
+    gld.when_due,
+    gld.when_modified,
+    gld.when_paid,
+	gld.customer,
+	gld.customer_id,
+	gld.vendor ,
+	gld.vendor_id,
+	gld.location,
+	gld.location_id
+    gld.when_paid,
+    gla.category,
+    gla.classification,
+    gla.account_type 
+
+    from gl_detail gld
+    left join gl_account gla
+    on gld.account_no = gla.account_no 
+)
+
+select *
 
     from general_ledger
