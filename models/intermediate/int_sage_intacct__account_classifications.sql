00ec0da0cee60a277495d451ae6dc4cbe6ecda35
fivetran-reneeli <renee.li@fivetran.com>
2022-02-17 16:43:34 -0800

    update classifications

--- models/intermediate/int_sage_intacct__account_classifications.sql
+++ models/intermediate/int_sage_intacct__account_classifications.sql
@@ -12,10 +12,10 @@ category,
 closing_account_title,
     case
         when category in ('Inventory','Fixed Assets','Other Current Assets','Cash and Cash Equivalents','Intercompany Receivable','Accounts Receivable','Deposits and Prepayments','Goodwill','Intangible Assets','Short-Term Investments','Inventory','Accumulated Depreciation','Other Assets','Unrealized Currency Gain/Loss','Patents','Investment in Subsidiary','Escrows and Reserves','Long Term Investments') then 'Asset'
-        when category in ('Partners Equity','Retained Earnings') then 'Equity'
-        when category in ('Other Operating Expense','Cost of Sales Revenue', 'Professional Services Expense','Cost of Services Revenue','Payroll Expense','Travel Expense','Cost of Goods Sold','Other Expenses','Compensation Expense','Federal Tax','Dividend Paid') then 'Expense'
-        when category in ('Accounts Payable','Other Current Liabilities','Payroll Taxes','Accrued Liabilities','Note Payable - Current','Deferred Taxes Liabilities - Long Term','Note Payable - Long Term','Other Liabilities','Deferred Revenue - Current') then 'Liability'
-        when category in ('Revenue - Sales','Dividend Income','Revenue - Other','Other Income','Revenue - Services','Revenue - Products') then 'Revenue'
+        when category in ('Partners Equity','Retained Earnings','Dividend Paid') then 'Equity'
+        when category in ('Advertising and Promotion Expense','Other Operating Expense','Cost of Sales Revenue', 'Professional Services Expense','Cost of Services Revenue','Payroll Expense','Payroll Taxes','Travel Expense','Cost of Goods Sold','Other Expenses','Compensation Expense','Federal Tax','Depreciation Expense') then 'Expense'
+        when category in ('Accounts Payable','Other Current Liabilities','Accrued Liabilities','Note Payable - Current','Deferred Taxes Liabilities - Long Term','Note Payable - Long Term','Other Liabilities','Deferred Revenue - Current') then 'Liability'
+        when category in ('Revenue','Revenue - Sales','Dividend Income','Revenue - Other','Other Income','Revenue - Services','Revenue - Products') then 'Revenue'
     end as classification 
 
 from gl_account
