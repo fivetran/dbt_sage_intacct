eba17f94ae235e3ea16d8e44cb59142ff3e5ec0e
fivetran-reneeli <renee.li@fivetran.com>
2022-02-24 16:00:15 -0800

    update with additional catchall

--- models/intermediate/int_sage_intacct__account_classifications.sql
+++ models/intermediate/int_sage_intacct__account_classifications.sql
@@ -8,7 +8,6 @@ select
 account_no,
 account_type,
 category,
--- cf_fs_group_3 as classification,
 closing_account_title,
     case
         when category in ('Inventory','Fixed Assets','Other Current Assets','Cash and Cash Equivalents','Intercompany Receivable','Accounts Receivable','Deposits and Prepayments','Goodwill','Intangible Assets','Short-Term Investments','Inventory','Accumulated Depreciation','Other Assets','Unrealized Currency Gain/Loss','Patents','Investment in Subsidiary','Escrows and Reserves','Long Term Investments') then 'Asset'
@@ -16,6 +15,11 @@ closing_account_title,
         when category in ('Advertising and Promotion Expense','Other Operating Expense','Cost of Sales Revenue', 'Professional Services Expense','Cost of Services Revenue','Payroll Expense','Payroll Taxes','Travel Expense','Cost of Goods Sold','Other Expenses','Compensation Expense','Federal Tax','Depreciation Expense') then 'Expense'
         when category in ('Accounts Payable','Other Current Liabilities','Accrued Liabilities','Note Payable - Current','Deferred Taxes Liabilities - Long Term','Note Payable - Long Term','Other Liabilities','Deferred Revenue - Current') then 'Liability'
         when category in ('Revenue','Revenue - Sales','Dividend Income','Revenue - Other','Other Income','Revenue - Services','Revenue - Products') then 'Revenue'
+        when (normal_balance ='credit' and account_type = 'balancesheet' and category not in ('Partners Equity','Retained Earnings','Dividend Paid')) then 'Liability'
+        when (normal_balance ='debit' and account_type = 'balancesheet') then 'Asset'
+        when (normal_balance ='credit' and account_type = 'incomestatement') then 'Revenue'
+        when (normal_balance ='debit' and account_type = 'incomestatement') then 'Expense'
+                
     end as classification 
 
 from gl_account
