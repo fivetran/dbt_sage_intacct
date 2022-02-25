c796b0122c04f6bad4ee202752f8aa206e760189
fivetran-reneeli <renee.li@fivetran.com>
2022-02-25 12:31:37 -0800

    update ap ar to use ap bill and ar invoice

--- models/sage_intacct__ap_ar_enhanced.sql
+++ models/sage_intacct__ap_ar_enhanced.sql
@@ -1,39 +1,92 @@
-with gl_detail as (
+with ap_bill as (
     select * 
-    from {{ ref('stg_sage_intacct__gl_detail') }} 
+    from {{ ref('stg_sage_intacct__ap_bill') }} 
 ),
 
-gl_account as (
+ap_bill_item as (
     select * 
-    from {{ ref('int_sage_intacct__account_classifications') }} 
+    from {{ ref('stg_sage_intacct__ap_bill_item') }} 
 ),
 
-enhanced as (
+ar_invoice as (
+    select * 
+    from {{ ref('stg_sage_intacct__ar_invoice') }} 
+),
+
+ar_invoice_item as (
+    select * 
+    from {{ ref('stg_sage_intacct__ar_invoice_item') }} 
+),
+
+-- with gl_detail as (
+--     select * 
+--     from {{ ref('stg_sage_intacct__gl_detail') }} 
+-- ),
+
+-- gl_account as (
+--     select * 
+--     from {{ ref('int_sage_intacct__account_classifications') }} 
+-- ),
+
+ap_bill_enhanced as (
 
     select
 
-	gld.transaction,
-	gld.entry_date,
-    gld.when_created,
-    gld.when_due,
-    gld.when_modified,
-    gld.when_paid,
-	gld.customer,
-	gld.customer_id,
-	gld.vendor,
-	gld.vendor_id,
-	gld.location,
-	gld.location_id,
-    gld.when_paid,
-    gla.category,
-    gla.classification,
-    gla.account_type 
+	ap_bill.
 
-    from gl_detail gld
-    left join gl_account gla
-    on gld.account_no = gla.account_no 
+
+
+    from ap_bill
+    -- left join gl_detail
+    --     on ap_bill.record_id = gl_detail.record_id
+    left join ap_bill_item 
+        on ap_bill.record_no = ap_bill_item.record_key
+),
+
+ar_invoice_enhanced as (
+
+    select 
+
+
+    from ar_invoice 
+    left join ar_invoice_item
+    on ar_invoice.record_no = ar_invoice_item.recordkey
+
+),
+
+ap_ar_enhanced as (
+
+    select * 
+    from ap_bill_enhanced
+    union 
+    select * 
+    from ar_invoice_enhanced
+
 )
 
-select 
-*
-from enhanced
+select * from ap_ar_enhanced
+
+
+
+
+/*
+AP/AR:
+- how much owed to each vendor
+- amount of time since invoice was created /  collect recievables
+- any past due (1 0 , days past due, days bw created/paid)
+
+
+Else:
+- whether it's a bill or invoice 
+- how many items per bill or invoice
+
+
+additional tables:
+- ap advance: currency, docnumber, documentnumber, financialaccount, 
+- ap advance item: departmentid, departmentkey, departmentname, exchange rate, line_no, lineitem, 
+- ap bill: due_in_days, 
+
+
+
+
+*/ 
