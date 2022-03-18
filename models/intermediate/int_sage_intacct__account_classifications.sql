2b6412adb036c237be045702c50cf206ff903c5d
fivetran-reneeli <renee.li@fivetran.com>
2022-03-18 15:50:17 -0500

    update

--- models/intermediate/int_sage_intacct__account_classifications.sql
+++ models/intermediate/int_sage_intacct__account_classifications.sql
@@ -15,11 +15,13 @@ final as (
             when category in {{ var('sage_intacct_category_expense') }} then 'Expense'
             when category in {{ var('sage_intacct_category_liability') }} then 'Liability'
             when category in {{ var('sage_intacct_category_revenue') }} then 'Revenue'
-            when (normal_balance = 'credit' and account_type = 'balancesheet' and category not in ('Partners Equity','Retained Earnings','Dividend Paid')) then 'Liability'
-            when (normal_balance = 'debit' and account_type = 'balancesheet') then 'Asset'
-            when (normal_balance = 'credit' and account_type = 'incomestatement') then 'Revenue'
-            when (normal_balance = 'debit' and account_type = 'incomestatement') then 'Expense'
-        end as classification 
+            when (normal_balance = 'debit' and account_type = 'balancesheet') and category not in {{ var('sage_intacct_category_asset') }} then 'Asset'
+            when (normal_balance = 'debit' and account_type = 'incomestatement') and category not in {{ var('sage_intacct_category_expense') }} then 'Expense'
+            when (normal_balance = 'credit' and account_type = 'balancesheet' and category not in {{ var('sage_intacct_category_liability') }} or category not in ('Partners Equity','Retained Earnings','Dividend Paid')) then 'Liability'
+            when (normal_balance = 'credit' and account_type = 'incomestatement') and category not in {{ var('sage_intacct_category_revenue') }} then 'Revenue'
+        end as classification,
+        normal_balance, 
+        title as account_title
 
         --The below script allows for pass through columns.
         {% if var('sage_account_pass_through_columns') %} 
--- models/intermediate/int_sage_intacct__general_ledger_balances.sql
+++ models/intermediate/int_sage_intacct__general_ledger_balances.sql
@@ -23,7 +23,7 @@ gl_period_balances as (
         sum(amount) as period_amount
     from general_ledger
     
-    {{ dbt_utils.group_by(9) }} 
+    {{ dbt_utils.group_by(10) }} 
 
 ), 
 
