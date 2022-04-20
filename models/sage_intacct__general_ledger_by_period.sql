75b25ae3fa8ad5cddff76f0a0aefb8aeaec82b75
fivetran-reneeli <renee.li@fivetran.com>
2022-04-20 13:52:03 -0500

    removed gl_detail pass through columns

--- models/sage_intacct__general_ledger_by_period.sql
+++ models/sage_intacct__general_ledger_by_period.sql
@@ -3,38 +3,82 @@ with general_ledger_balances as (
     from {{ ref('int_sage_intacct__general_ledger_balances') }}
 ), 
 
-final as (
+general_ledger_by_period as (
     select 
+        period_first_day,
+        period_last_day,
+        date_year, 
         account_no,
         account_title,
+        account_type,
         book_id,
         category,
         classification,
         currency,
-        entry_state,
-        account_type,
-        date_year, 
-        period_first_day,
-        period_last_day
+        entry_state
 
-        {% if var('sage_account_pass_through_columns') %} 
-        , 
-        {{ var('sage_account_pass_through_columns') | join (", ")}}
+        -- {% if var('sage_account_pass_through_columns') %} 
+        -- , 
+        -- {{ var('sage_account_pass_through_columns') | join (", ")}}
 
-        {% endif %}
-
-        {% if var('sage_gl_pass_through_columns') %}
-        ,     
-        {{ var('sage_gl_pass_through_columns') | join (", ")}} 
-        
-        {% endif %}
+        -- {% endif %}
         ,
         round(cast(period_net_amount as {{ dbt_utils.type_numeric() }}),2) as period_net_amount,
         round(cast(period_beg_amount as {{ dbt_utils.type_numeric() }}),2) as period_beg_amount,
         round(cast(period_ending_amount as {{ dbt_utils.type_numeric() }}),2) as period_ending_amount
     from general_ledger_balances
+),
+
+general_ledger_by_period_retained_earnings_tmp as (
+    select
+        period_first_day,
+        period_last_day,
+        date_year, 
+        'Adj. Net Income' as account_no,
+        'Retained Earnings' as account_title,
+        'balancesheet' as account_type,
+        'ACCRUAL' as book_id,
+        'Retained Earnings' as category,
+        'Equity' as classification,
+        'currency' as currency,
+        'P' as entry_state,
+        sum(period_net_amount) as period_net_amount,
+        sum(period_beg_amount) as period_beg_amount,
+        sum(period_ending_amount) as period_ending_amount
+    from general_ledger_by_period
+    where account_type = 'incomestatement'
+    group by 1,2,3
+),
+
+general_ledger_by_period_retained_earnings as (
+    select
+        period_first_day,
+        period_last_day,
+        date_year, 
+        'Adj. Net Income' as account_no,
+        'Retained Earnings' as account_title,
+        'balancesheet' as account_type,
+        'ACCRUAL' as book_id,
+        'Retained Earnings' as category,
+        'Equity' as classification,
+        'currency' as currency,
+        'P' as entry_state,
+        null as period_net_amount,
+        null as period_beg_amount,
+        sum(period_net_amount) over (
+            order by period_first_day
+        ) as period_ending_amount
+    from general_ledger_by_period_retained_earnings_tmp
+    order by period_first_day
+),
+
+final as (
+    select *
+    from general_ledger_by_period
+    union all
+    select *
+    from general_ledger_by_period_retained_earnings
 )
 
 select * 
 from final 
-
