8b731c0acbaae29164f5cda73bf6cfad58b996a4
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-06-26 14:15:25 -0500

    feature/freshness-updates (#30)
    * feature/freshness-updates
    
    * Update packages.yml
    
    ---------
    
    Co-authored-by: fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -17,6 +17,7 @@ echo `pwd`
 cd integration_tests
 dbt deps
 dbt seed --target "$db" --full-refresh
+dbt source freshness --target "$db" || echo "...Only verifying freshness runs…"
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
 dbt run --vars '{sage_intacct__using_invoices: false, sage_intacct__using_bills: false}' --full-refresh --target "$db"
